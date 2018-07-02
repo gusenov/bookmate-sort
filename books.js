97fb6a4aaa13cc439a28e41dd1037b6f6792c162
Аббас Гусенов <gusenov@live.ru>
2018-07-02 22:30:05 +0600

    Version 0.0.0.5

--- books.js
+++ books.js
@@ -1,13 +1,140 @@
+function getKind(item) {
+  if ('labels' in item
+    && Array.isArray(item.labels)
+    && item.labels.length > 0
+    && 'kind' in item.labels[0]) {
+      return item.labels[0].kind;
+
+  } else if ('book' in item
+    && 'labels' in item.book
+    && Array.isArray(item.book.labels)
+    && item.book.labels.length > 0
+    && 'kind' in item.book.labels[0]) {
+      return item.book.labels[0].kind;
+  }
+
+  return "";
+}
+
+function isPremium(item) {
+  if (getKind(item).localeCompare('premium') === 0) {
+    return true;
+  }
+  return false;
+}
+
+function canBeRead(item) {
+  if ('can_be_read' in item) {
+    return item.can_be_read;
+
+  } else if ('book' in item
+    && 'can_be_read' in item.book) {
+      return item.book.can_be_read;
+  }
+
+  return false;
+}
+
+function getLanguage(item) {
+  if ('lang' in item) {
+    return item.lang;
+
+  } else if ('book' in item
+    && 'language' in item.book) {
+    return item.book.language;
+  }
+
+  return "";
+}
+
+function isRussian(item) {
+  if (getLanguage(item).localeCompare('ru') === 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function isEnglish(item) {
+  if (getLanguage(item).localeCompare('en') === 0) {
+    return true;
+  }
+
+  return false;
+}
+
+function getAuthors(item) {
+  if ('authors' in item) {
+    return item.authors;
+
+  } else if ('book' in item
+    && 'authors' in item.book) {
+      return item.book.authors;
+  }
+
+  return "";
+}
+
+function getPaperPages(item) {
+  if ('paper_pages' in item) {
+    return item.paper_pages;
+
+  } else if ('book' in item
+    && 'paper_pages' in item.book) {
+      return item.book.paper_pages;
+  }
+
+  return 0;
+}
+
+function getBookUuid(item) {
+  if ('book_uuid' in item) {
+    return item.book_uuid;
+
+  } else if ('book' in item
+    && 'uuid' in item.book) {
+      return item.book.uuid;
+  }
+
+  return "";
+}
+
+function getState(item) {
+  if ('lc' in item
+    && 'state' in item.lc) {
+      return item.lc.state;
+
+  } else if ('state' in item) {
+    return item.state;
+  }
+
+  return "";
+}
+
+function getTitle(item) {
+  if ('title' in item
+    && item.title.trim()) {
+      return item.title;
+
+  } else if ('book' in item
+    && 'title' in item.book) {
+      return item.book.title;
+  }
+
+  return "";
+}
+
+function getReadersCount(item) {
+  if ('book' in item
+    && 'readers_count' in item.book) {
+      return item.book.readers_count;
+  }
+
+  return -1;
+}
+
 function createTbl(books, type) {
-    var item,
-        itemAuthors,
-        itemState,
-        itemKind,
-        itemPaperPages,
-        itemUrl,
-        itemCanBeRead,
-        itemLanguage,
-        itemReadersCount;
+    var item, itemState, itemReadersCount;
 
     var tbl = document.createElement('table');
     tbl.style.width = '100%';
@@ -18,51 +145,30 @@ function createTbl(books, type) {
     for (var i = 0; i < books.length; i++) {
         item = books[i];
 
-        itemCanBeRead = 'can_be_read' in item ? item.can_be_read : item.book.can_be_read;
-        if (itemCanBeRead === false) { continue; }
-        
-        itemKind = ('labels' in item && 'kind' in item.labels[0]) ? item.labels[0].kind : item.book.labels[0].kind;
-        if (type.localeCompare('premium') === 0 && itemKind.localeCompare('premium') !== 0) { continue; }
-        
-        itemLanguage = 'lang' in item ? item.lang : item.book.language;
-        if (type.localeCompare('ru') === 0) {
-            if (!itemLanguage || itemLanguage.localeCompare('ru') !== 0) {
-                continue;
-            }
-        } else if (type.localeCompare('en') === 0) {
-            // The following table is ordered from highest (20) to lowest (0) precedence.
-            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
-            // 5  ||
-            // 6  &&
-            // 10 !==
-            // 16 !
-            if (!itemLanguage || itemLanguage && itemLanguage.localeCompare('en') !== 0) {
-                continue;
-            }
-        }
+        if (!canBeRead(item)) { continue; }
+        if (type.localeCompare('premium') === 0 && !isPremium(item)) { continue; }
+        if (type.localeCompare('ru') === 0 && !isRussian(item)) { continue;
+        } else if (type.localeCompare('en') === 0 && !isEnglish(item)) { continue; }
 
         tr = document.createElement('tr');
 
         td = document.createElement('td');
-        itemAuthors = ('authors' in item) ? item.authors : item.book.authors;
-        td.appendChild(document.createTextNode(itemAuthors));
+        td.appendChild(document.createTextNode(getAuthors(item)));
         tr.appendChild(td);
 
         a = document.createElement('a');
-        itemUrl = 'https://bookmate.com/books/' + ('book_uuid' in item ? item.book_uuid : item.book.uuid);
-        a.setAttribute('href', itemUrl);
-        a.innerHTML = item.title;
+        a.setAttribute('href', 'https://bookmate.com/books/' + getBookUuid(item));
+        a.innerHTML = getTitle(item);
         td = document.createElement('td');
         td.appendChild(a);
         tr.appendChild(td);
 
         td = document.createElement('td');
-        itemPaperPages = ('paper_pages' in item) ? item.paper_pages : item.book.paper_pages;
-        td.appendChild(document.createTextNode(itemPaperPages));
+        td.appendChild(document.createTextNode(getPaperPages(item)));
         tr.appendChild(td);
 
         td = document.createElement('td');
-        itemState = ('lc' in item && 'state' in item.lc) ? item.lc.state : item.state;
+        itemState = getState(item);
         switch (itemState) {
             case 'pending':
                 itemState = 'to-read';
@@ -75,11 +181,11 @@ function createTbl(books, type) {
         tr.appendChild(td);
 
         td = document.createElement('td');
-        td.appendChild(document.createTextNode(itemKind));
+        td.appendChild(document.createTextNode(getKind(item)));
         tr.appendChild(td);
-        
-        if ('book' in item && 'readers_count' in item.book) {
-            itemReadersCount = item.book.readers_count;
+
+        itemReadersCount = getReadersCount(item);
+        if (itemReadersCount >= 0) {
             td = document.createElement('td');
             td.appendChild(document.createTextNode(itemReadersCount));
             tr.appendChild(td);
@@ -95,7 +201,7 @@ function loadBooks(userChoice){
     var allBooks = [],
         booksLoaded,
         pageIndex = 1;
-    
+
     function getEndpoint() {
         var fileName;
         switch (userChoice) {
@@ -111,7 +217,7 @@ function loadBooks(userChoice){
         }
         return 'https://bookmate.com/a/4/u/d/' + fileName + '.json?page=' + pageIndex + '&p=' + pageIndex + '&per_page=256';
     }
-  
+
     SeqExec.loop(function loopBody(cont) {
         getJsonAsObj(getEndpoint(), function (books) {
             switch (userChoice) {
@@ -151,7 +257,7 @@ function loadBooks(userChoice){
 document.addEventListener("DOMContentLoaded", function () {
     try {
         var currentUrl = window.location.href;
-        
+
         if (currentUrl.endsWith('#all')) {
             loadBooks('all');
             document.title = "all";
@@ -165,20 +271,20 @@ document.addEventListener("DOMContentLoaded", function () {
             loadBooks('finished');
             document.title = "read";
         }
-        
+
         else if (currentUrl.endsWith('#en')) {
             loadBooks('en');
             document.title = "en";
         } else if (currentUrl.endsWith('#ru')) {
             loadBooks('ru');
             document.title = "ru";
-        } 
-        
+        }
+
         else if (currentUrl.endsWith('#premium')) {
             loadBooks('premium');
             document.title = "premium";
         }
-        
+
         else if (currentUrl.endsWith('#readers-count')) {
             loadBooks('readers-count');
             document.title = "readers-count";
